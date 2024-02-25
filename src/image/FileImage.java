b1ee93b39a23d4d40f9d50ca8fee653b3d6d1864
itaedel <ita.edel@protonmail.com>
2024-02-25 16:43:33 +0200

    Project

--- src/image/FileImage.java
+++ src/image/FileImage.java
@@ -2,8 +2,10 @@ package image;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -13,41 +15,75 @@ import java.util.NoSuchElementException;
  */
 class FileImage implements Image {
     private static final Color DEFAULT_COLOR = Color.WHITE;
-    private static final int BASE = 2;
+    private static final int BASE_2 = 2;
     private final int height;
     private final int width;
     private final Color[][] pixelArray;
+//    private final BufferedImage bufferedImage;
+//    private final int origWidth;
+//    private final int origHeight;
+//    private final int heightDiff;
+//    private final int widthDiff;
 
 
     /**
      * constructor
      * @param filename the filename
-     * @throws IOException in case of fail?
+     * @throws IOException in case of fail
      */
     public FileImage(String filename) throws IOException {
         java.awt.image.BufferedImage im = ImageIO.read(new File(filename));
         int origWidth = im.getWidth(), origHeight = im.getHeight();
         //im.getRGB(x, y)); getter for access to a specific RGB rates
-        this.width = (int) Math.pow(2, Math.floor(Math.log(origWidth) / Math.log(2)) + 1.0);
-        this.height = (int) Math.pow(2, Math.floor(Math.log(origHeight) / Math.log(2)) + 1.0);
-        int heightDiff = (height - origHeight) / 2, widthDiff = (width - origWidth) / 2;
+        if(logBaseChange(origWidth, BASE_2) == Math.floor(logBaseChange(origWidth,BASE_2))){
+            this.width = origWidth;
+        } else {
+            this.width = (int)Math.pow(BASE_2, Math.floor(logBaseChange(origWidth, BASE_2) + 1));
+        }
+        if(logBaseChange(origHeight, BASE_2) == Math.floor(logBaseChange(origHeight, BASE_2))){
+            this.height = origHeight;
+        } else {
+            this.height = (int)Math.pow(BASE_2, Math.floor(logBaseChange(origHeight, BASE_2) + 1));
+        }
         pixelArray = new Color[width][height];
+        fillPixelArray(origWidth, origHeight, im); // fill array
+    }
+
+    /**
+     * fills the pixels array and adds padding
+     * @param origWidth the image original width
+     * @param origHeight the image original height
+     * @param im the image itself
+     */
+    private void fillPixelArray(int origWidth, int origHeight,
+                                BufferedImage im){
+        int heightDiff = (height - origHeight) / 2;
+        int widthDiff = (width - origWidth) / 2;
         for(int i = 0; i < width; i++){
             for (int j = 0; j < height; j++) {
                 if(i < origWidth + widthDiff && j < origHeight + heightDiff &&
                         i >= widthDiff && j >= heightDiff){ // for padding edges
                     pixelArray[i][j] = new Color(im.getRGB(i - widthDiff, j - heightDiff));
                 } else{
-                    pixelArray[i][j] = DEFAULT_COLOR;
+                    pixelArray[i][j] = DEFAULT_COLOR; // pad edges
                 }
             }
-
         }
     }
 
     /**
+     * transform base of log
+     * @param a the result
+     * @param b new log base
+     * @return log_a^b
+     */
+    private double logBaseChange(double a, double b){
+        return Math.log(a) / Math.log(b);
+    }
+
+    /**
      * getter function
-     * @return returns width
+     * @return returns image width
      */
     @Override
     public int getWidth() {
@@ -76,83 +112,58 @@ class FileImage implements Image {
         }
         return null;
     }
-
-    /**
-     * iterator that goes over sub-images from in order of rows from first row to last while going over
-     * each row from first column to last
-     * @param sub_image_size the size of each given image - the n in the n x n
-     * @return the special iterator
-     */
-    @Override
-    public Iterable<Color> pixels(int sub_image_size) {
-        return new IterSubImage<Color>(this, sub_image_size);
-    }
 }
 
+
 /**
- * special iterator for going over the sub-images one by one in order of row.
+ * reperesnt a sub image as an image object
  */
-class IterSubImage<Color> implements Iterable<Color>{
-    private final Image image;
-    private final int sub_image_size; // the n in n X n
-    private Color color;
+class SubImage implements Image{
+    private final Color[][] image;
+    private final int width;
+    private final int height;
+
 
     /**
      * constructor
-     * @param image
-     * @param sub_image_size
+     * @param image the pixels of the image
+     * @param width the width
+     * @param height the height
      */
-    public IterSubImage(Image image, int sub_image_size){
+    public SubImage(Color[][] image, int width, int height) {
         this.image = image;
-        this.sub_image_size = sub_image_size;
+        this.width = width;
+        this.height = height;
     }
 
     /**
-     * returns the iterator object that can be iterated to go over sub images
-     * @return an Iterator<Color>
+     * get pixel at x,y
+     * @param x
+     * @param y
+     * @return the pixel
      */
     @Override
-    public Iterator<Color> iterator() {
-        return new Iterator<Color>() {
-            int x = 0, y = 0;
-            int sub_image_row = 0, sub_image_col = 0; // thinking of the array as a 2d array of sub-pictures
-
-            /**
-             * checks if there are more pixels to go over
-             * @return true if there are, false otherwise
-             */
-            @Override
-            public boolean hasNext() {
-                return (sub_image_row * sub_image_size) < image.getHeight();
-            }
-
-            /**
-             * returns the current pixel and advances to the next
-             * @return a single pixel Color
-             */
-            @Override
-            public Color next() {
-                if(!hasNext()){
-                    throw new NoSuchElementException();
-                }
-                var cur_pixel = image.getPixel(sub_image_col* sub_image_size + x,
-                        sub_image_row * sub_image_size + y);
-                x += 1;
-                if(x >= sub_image_size){
-                    x = 0;
-                    y++;
-                }
-                if(y >= sub_image_size){
-                    y = 0;
-                    sub_image_col++;
-                }
-                if(sub_image_col * sub_image_size >= image.getWidth()){
-                    sub_image_col = 0;
-                    sub_image_row++;
-                }
-                return (Color) cur_pixel; // todo why is this bad?
-            }
-        };
+    public Color getPixel(int x, int y) {
+        return image[x][y];
     }
 
+    /**
+     * get width of an image
+     * @return
+     */
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * get height of an image
+     * @return
+     */
+    @Override
+    public int getHeight() {
+        return height;
+    }
 }
+
+
