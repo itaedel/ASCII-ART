644520e53002fc9bada76cae0420ce22c71569e9
Itamar Edelstein <77614805+itaedel@users.noreply.github.com>
2024-06-16 12:15:07 +0300

    Update Image.java

--- src/image/Image.java
+++ src/image/Image.java
@@ -75,41 +75,4 @@ public interface Image {
         arrayList.add(new SubImage(temp, subImageSize, subImageSize)); //adds the last one
         return arrayList;
     }
-
-
-//    /**
-//     * default method to parcel image to sub images and return them
-//     * @param subImageSize the size of each sub image
-//     * @return an ArrayList that holds all the sub images
-//     */
-//    default ArrayList<Color[][]> divToSubImage(int subImageSize){
-//        ArrayList<Color[][]> subImages = new ArrayList<Color[][]>();
-//        int x = 0, y = 0, subImageRow = 0, subImageCol = 0, count = 0;
-//        Color [][] temp = new Color[subImageSize][subImageSize];
-//        while ((subImageRow * subImageSize) < this.getHeight()){
-//            if(count >= subImageSize * subImageSize){
-//                count = 0;
-//                subImages.add(temp);
-//                temp = new Color[subImageSize][subImageSize];
-//            }
-//            count++;
-//            temp[x][y] = this.getPixel(subImageCol * subImageSize + x,
-//                    subImageRow * subImageSize + y);
-//            x += 1;
-//            if(x >= subImageSize){
-//                x = 0;
-//                y++;
-//            }
-//            if(y >= subImageSize){
-//                y = 0;
-//                subImageCol++;
-//            }
-//            if(subImageCol * subImageSize >= this.getWidth()){
-//                subImageCol = 0;
-//                subImageRow++;
-//            }
-//        }
-//        subImages.add(temp);
-//        return subImages;
-//    }
 }
